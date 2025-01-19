950e2b0e69be9dbb996b6cf3db93d9beb3ed0a33
sykfeuil <syk.devmail@gmail.com>
2025-01-19 19:06:53 +0100

    Route services

--- router/allRoutes.js
+++ router/allRoutes.js
@@ -2,7 +2,8 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html", [])
+    new Route("/", "Accueil", "/pages/home.html", []),
+    new Route("/Services", "Nos services", "pages/service.html", [])
 ];
 
 
