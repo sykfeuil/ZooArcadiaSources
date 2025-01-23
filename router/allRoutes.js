f7bf4156e0e28d0b0304ed89026d2e4745b17a09
sykfeuil <syk.devmail@gmail.com>
2025-01-23 22:43:50 +0100

    Page connexion front

--- router/allRoutes.js
+++ router/allRoutes.js
@@ -3,10 +3,11 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html", []),
-    new Route("/services", "Nos services", "pages/service.html", [], "js/service.js"),
-    new Route("/noshabitats", "Nos Habitats", "pages/allHabitats.html", [], "js/allHabitats.js"),
-    new Route("/habitat", "Habitat", "/pages/habitat.html", [], "js/habitat.js"),
-    new Route("/avis", "Laisser un avis", "/pages/rating.html", [])
+    new Route("/services", "Nos services", "pages/data/service.html", [], "js/service.js"),
+    new Route("/noshabitats", "Nos Habitats", "pages/data/allHabitats.html", [], "js/allHabitats.js"),
+    new Route("/habitat", "Habitat", "/pages/data/habitat.html", [], "js/habitat.js"),
+    new Route("/avis", "Laisser un avis", "/pages/user/rating.html", []),
+    new Route("/connexion", "Connexion", "/pages/user/connection.html", [])
 ];
 
 
