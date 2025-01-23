f4233aa9a895486f0a9c027643d62c7ebaccfb30
sykfeuil <syk.devmail@gmail.com>
2025-01-23 22:17:55 +0100

    Page avis front

--- router/allRoutes.js
+++ router/allRoutes.js
@@ -5,7 +5,8 @@ export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html", []),
     new Route("/services", "Nos services", "pages/service.html", [], "js/service.js"),
     new Route("/noshabitats", "Nos Habitats", "pages/allHabitats.html", [], "js/allHabitats.js"),
-    new Route("/habitat", "NomHabitat", "/pages/habitat.html", [], "js/habitat.js")
+    new Route("/habitat", "Habitat", "/pages/habitat.html", [], "js/habitat.js"),
+    new Route("/avis", "Laisser un avis", "/pages/rating.html", [])
 ];
 
 
