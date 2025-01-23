5a73fa86099711087908104bcca38168f9422c08
sykfeuil <syk.devmail@gmail.com>
2025-01-23 22:50:51 +0100

    Page contact front

--- router/allRoutes.js
+++ router/allRoutes.js
@@ -7,7 +7,8 @@ export const allRoutes = [
     new Route("/noshabitats", "Nos Habitats", "pages/data/allHabitats.html", [], "js/allHabitats.js"),
     new Route("/habitat", "Habitat", "/pages/data/habitat.html", [], "js/habitat.js"),
     new Route("/avis", "Laisser un avis", "/pages/user/rating.html", []),
-    new Route("/connexion", "Connexion", "/pages/user/connection.html", [])
+    new Route("/connexion", "Connexion", "/pages/user/connection.html", []),
+    new Route("/contact", "Contactez-nous", "/pages/user/contact.html", [])
 ];
 
 
